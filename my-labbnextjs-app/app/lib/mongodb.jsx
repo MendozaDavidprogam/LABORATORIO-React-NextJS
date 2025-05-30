489667845a03c30ab60f39fc927f5078fbba25b7
MendozaDavidprogam <mendozadavidprogramacion2.0@gmail.com>
2025-05-29 20:37:14 -0400

    d

--- my-labbnextjs-app/app/lib/mongodb.jsx
+++ my-labbnextjs-app/app/lib/mongodb.jsx
@@ -40,7 +40,7 @@ import mongoose from 'mongoose';
 const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
-  throw new Error('Por favor define la variable MONGODB_URI en las variables de entorno');
+  throw new Error('Por favor defina la variable MONGODB_URI en las variables de entorno');
 }
 
 let cached = global.mongoose;
@@ -57,12 +57,14 @@ async function connectToDatabase() {
   if (!cached.promise) {
     const opts = {
       bufferCommands: false,
+      // Puedes agregar otras opciones aquí según tus necesidades
     };
 
     cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
       return mongoose;
     });
   }
+
   cached.conn = await cached.promise;
   return cached.conn;
 }
